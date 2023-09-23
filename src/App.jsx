eb56d283d42d10bc9fa3614b2fcc8042e064eef5
kainat <anmolkainat244@gmail.com>
2023-09-23 12:31:05 +0500

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Browser, Routes, Route, Link } from "react-router-dom";
+import { logo } from "./assets";
+import { Home, CreatePost } from "./pages";
 
 const App = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
